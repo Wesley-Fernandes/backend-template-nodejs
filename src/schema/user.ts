3f9307074bbb3d66f0f6fd8facec86f6dd447021
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-05 20:40:03 -0300

    featur: schemas user and middleware

---
+++ src/schema/user.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+export const userSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8).max(25),
+  username: z.string().min(2).max(25),
+  birth: z.string().datetime(),
+});
+
+export const userLoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8).max(25),
+});
